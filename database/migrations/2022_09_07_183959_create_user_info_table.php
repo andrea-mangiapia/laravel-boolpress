cca9818c0d054ae558609b989a0aa4ba48e98dc7
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-09-07 22:52:20 +0200

    done relations 1to1

---
+++ database/migrations/2022_09_07_183959_create_user_info_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('telephone', 20);
+            $table->string('address', 255);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            // Dire che user_id è una foreign key
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_info');
+    }
+}
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,5 +7,13 @@
     <div>
         Benvenuto {{ $user->name }}, la tua email è {{ $user->email }}
     </div>
+    @if ($user->userInfo)
+        <div>
+            Nunero di telefono: {{ $user->userInfo->telephone }}
+        </div>
+        <div>
+            Indirizzo: {{ $user->userInfo->address }}
+        </div>       
+    @endif
     
 @endsection
