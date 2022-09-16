dd8ae0eef13b94ca0e73194ebb9aae066b0a8fb5
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-09-16 16:29:54 +0200

    refactoring postDetails

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -6,17 +6,8 @@
 
                 <div class="row row-cols-3">
                     <!-- single col -->
-                    <div v-for="post in posts" :key="post.id" class="col">
-                        <div class="card mt-3" style="width: 18rem;">
-                            <!-- <img src="..." class="card-img-top" alt="..."> -->
-                            <div class="card-body">
-                                <h5 class="card-title">{{post.title}}</h5>
-                                <p class="card-text">{{truncateText(post.content)}}</p>
-                                <router-link class="btn btn-primary" :to="{name: 'single-post', params: { slug: post.slug }}">
-                                    Leggi
-                                </router-link>
-                            </div>
-                        </div>
+                    <div v-for="singlePost in posts" :key="singlePost.id" class="col">
+                        <PostDetails :post="singlePost" />
                     </div>
                 </div>
 
@@ -43,34 +34,29 @@
 </template>
 
 <script>
-// import axios from 'axios';
-// import { getProcessArgvWithoutBin } from 'yargs/build/lib/process-argv';
+import PostDetails from './PostDetails.vue';
 
 export default {
-    name: 'Posts',
+    name: "Posts",
+    components: { 
+        PostDetails
+    },
     data() {
         return {
-            title: 'Tutti i post',
+            title: "Tutti i post",
             posts: [],
             currentPaginationPage: 1,
             lastPaginationPage: null
         };
     },
     methods: {
-        truncateText(text) {
-            if(text.length > 75) {
-                return text.slice(0, 75) + '...';
-            }
-
-            return text;
-        },
         getPosts(pageNumber) {
-            axios.get('/api/posts', {
+            axios.get("/api/posts", {
                 params: {
                     page: pageNumber
                 }
             })
-            .then((response) => {
+                .then((response) => {
                 // console.log(response.data.results);
                 this.posts = response.data.results.data;
                 this.currentPaginationPage = response.data.results.current_page;
@@ -79,8 +65,8 @@ export default {
         }
     },
     mounted() {
-        this.getPosts();        
-
-    }
+        this.getPosts();
+    },
+    
 }
 </script>
